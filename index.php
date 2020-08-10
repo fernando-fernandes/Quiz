13551f069c2bba3c92bc071b292eb1b60103f9eb
fernando-fernandes <fernando.85@outlook.com>
2020-08-09 23:50:40 -0300

    Fontes

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="theme-color" content="#114FA8">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="fonts/css/all.min.css">  
-        <link rel="stylesheet" href="css/styles.min.css">    
-        <!-- <link rel="stylesheet" href="assets/css/index.css"> -->
-        <title>Quiz</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#114FA8">
 
-    </head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fonts/css/all.min.css">
+    <link rel="stylesheet" href="css/styles.min.css">
+    <!-- <link rel="stylesheet" href="assets/css/index.css"> -->
+    <title>Quiz</title>
 
-    <?php
-        /*include_once 'open_db.php';  
+</head>
+
+<?php
+/*include_once 'open_db.php';  
         $id = 1;  
         $sql = "select * from tracking where id_usuario = ".$id;
         $result = mysqli_query($conn, $sql);
@@ -28,15 +29,22 @@
             "(".$id.", 'A','A','F',0,'F')";
             $result = mysqli_query($conn, $sql);
         }*/
-    ?>
+?>
 
-    <body>
+<body>
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-3 header">
-                <div class="logo d-flex justify-content-center">
+            <div class="col-12 col-md-3">
+                <img src="images/Logo-mobile.svg" alt="">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <!-- <div class="logo d-flex justify-content-center">
                     <img src="assets/img/logo_mobil.png" width="50%" alt="" srcset="">
-                </div>
-                <div class="structures d-flex justify-content-center">
+                </div> -->
+                <div class="containerNav d-flex justify-content-center">
                     <ul>
                         <li><?= $m0_icone ?><a href="index.php?p=m0"> Home</a></li>
                         <li><?= $m1_icone ?><a href="index.php?p=m1"> Módulo 1</a></li>
@@ -56,53 +64,58 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-9 quizSection d-flex justify-content-center">
+
+            <div class="col-12 col-md-9 quizSection d-flex justify-content-center">
                 <div class="quizCard shadow ">
-                    <?php                                        
-                        // $par = $_GET["p"];
-                        // switch ($par) {
-                        //     case "m0":
-                        //         include_once("home.html");
-                        //         break;
-                        //     case "m1":
-                        //         include_once("mod1.php");
-                        //         break;
-                        //     case "m1a":
-                        //         include_once("mod1a.php");
-                        //         break;                                
-                        //     case "m2":
-                        //         include_once("mod2.php");
-                        //         break;
-                        //     case "m2a":
-                        //         include_once("mod2a.php");
-                        //         break;                                
-                        //     case "e1":
-                        //         include_once("extra1.php");
-                        //         break;
-                        //     case "e1a":
-                        //         include_once("quizz.php");
-                        //         break;
-                        //     case "e1f":
-                        //         include_once("quizz-feedback.php");
-                        //         break;
-                        //     case "e1q":
-                        //         include_once("quizz-quadro.php");
-                        //         break;
-                        //     case "m3":
-                        //         include_once("mod3.html");
-                        //         break;
-                        //     default:
-                        //         include_once("home.html");
-                        //         break;
-                        // }
+                    <?php
+                    // $par = $_GET["p"];
+                    // switch ($par) {
+                    //     case "m0":
+                    //         include_once("home.html");
+                    //         break;
+                    //     case "m1":
+                    //         include_once("mod1.php");
+                    //         break;
+                    //     case "m1a":
+                    //         include_once("mod1a.php");
+                    //         break;                                
+                    //     case "m2":
+                    //         include_once("mod2.php");
+                    //         break;
+                    //     case "m2a":
+                    //         include_once("mod2a.php");
+                    //         break;                                
+                    //     case "e1":
+                    //         include_once("extra1.php");
+                    //         break;
+                    //     case "e1a":
+                    //         include_once("quizz.php");
+                    //         break;
+                    //     case "e1f":
+                    //         include_once("quizz-feedback.php");
+                    //         break;
+                    //     case "e1q":
+                    //         include_once("quizz-quadro.php");
+                    //         break;
+                    //     case "m3":
+                    //         include_once("mod3.html");
+                    //         break;
+                    //     default:
+                    //         include_once("home.html");
+                    //         break;
+                    // }
                     ?>
                 </div>
             </div>
         </div>
+    </div>
 
 
-        <script src="js/jquery-3.5.1.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/main.js"></script>
-    </body>
+
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
 </html>
