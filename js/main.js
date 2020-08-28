98cce4ab1bd2f0b1390d2323b9da0a77f20991b0
fernando-fernandes <fernando.85@outlook.com>
2020-08-28 01:13:12 -0300

    scss atual

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 
-
-    $('.nav-disabled a').click(function(){
+    // Sidebar
+    $('.nav-disabled').click(function(){
         event.preventDefault();
-    })
+    });
 
+    $('.sidebar-btn').click(function(){
+        $('.sidebar, .content').toggleClass('active');
+    });
 
+    // Ajustar largura PPT
+    (function mostrarPPT(){
+        var container = $('.container-ppt');
+        var quizcard = $('.quizCard');
+
+        if(quizcard.is(':visible')) {
+            container.width('100%');
+            $('.content').css('padding','60px 0 20px')
+        } else {
+            $('.content').css('padding','0')
+        }
+    })();
         
-    // $(".sidebar").mCustomScrollbar({
-    //     theme: "minimal"
-    // });
